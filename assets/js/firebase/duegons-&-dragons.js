da13c51b496a7882a90f3afedfe4cb4f60155a80
Bianca Miranda <miranda.bianca41@gmail.com>
2022-09-12 20:06:17 -0300

    .

--- assets/js/firebase/duegons-&-dragons.js
+++ assets/js/firebase/duegons-&-dragons.js
@@ -1,5 +1,6 @@
 import app from './app.js'
 import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js'
+
 export async function subscribeClub (subscription){
     const db = getFirestore(app)
     const clubCollection = collection(db, 'duegons-e-dragons')
